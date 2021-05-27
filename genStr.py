6f51b614590dc79de06ac4a5fb563d1902db96ec
CGSL-TEAM <84677254+CGSL-TEAM@users.noreply.github.com>
2021-05-27 08:02:31 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -13,7 +13,7 @@ from pyrogram.errors import (
 )
 
 API_TEXT = """Hi, {}.
-This is Pyrogram's String Session Generator Bot. I will generate String Session of your Telegram Account.help groups in https://t.me/cgs_technology and https://t.me/SL_TECH_MIDEA_GROUP
+This is Pyrogram's String Session Generator Bot. I will generate String Session of your Telegram Account.help groups in @cgs_technology and @SL_TECH_MEDIA_GROUP
 
 By @kmsrk
 
